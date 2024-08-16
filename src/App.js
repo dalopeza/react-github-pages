f8493cb6e390eefc50cf5257f5f1870e6cf1f89c
Dany López Z. <djvamps2007@gmail.com>
2024-08-16 17:03:56 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,11 +7,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Agencia de <code>Marketing</code> Pengrafic.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://pengrafic.com"
           target="_blank"
           rel="noopener noreferrer"
         >
